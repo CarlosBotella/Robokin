c9efdbb77cc80bb8e1d88931e8c7aca70635f184
Eduard <eduard01kate@gmail.com>
2024-04-29 05:42:39 +0200

    RK-H2-00 ROS

---
+++ ROS/build/robokin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eduard/Escritorio/Robokin/ROS/robokin/robokin
+# Build directory: /home/eduard/Escritorio/Robokin/ROS/build/robokin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
